d00634e3098c14f2cc4953a4f231ae74392aad36
Alaa Jakoub <jakoubalaa11@gmail.com>
2025-12-10 22:12:09 +0300

    small frame and grid it |_ figure out how to draw rects

--- src/main/java/Ess/Panel.java
+++ src/main/java/Ess/Panel.java
@@ -6,9 +6,9 @@ import java.awt.*;
 public class Panel extends JPanel implements Runnable{
     //1024 x 1280
     protected final int tile_size = 32 ;
-    protected final int scale_ratio = 4 ;
+    protected final int scale_ratio = 2 ;
     protected final int tile = tile_size * scale_ratio ;
-    protected final int screen_col = 10 ;
+    protected final int screen_col = 6 ;
     protected final int screen_row = 6 ;
     protected final int screenWidth = screen_col * tile ;
     protected final int screenHeight = screen_row * tile ;
@@ -18,6 +18,8 @@ public class Panel extends JPanel implements Runnable{
     protected final int pause = 1 ;
     protected final int playing = 2 ;
     public Thread thread =new Thread(this);
+
+    public Rectangle [] rectangle_arr =new Rectangle[3];
     //constructor
     public Panel () {
         setPreferredSize(new Dimension(screenWidth,screenHeight));
@@ -36,6 +38,15 @@ public class Panel extends JPanel implements Runnable{
         super.paintComponent(g);
         Graphics2D g2d= (Graphics2D) g;
 
+        g2d.setStroke(new BasicStroke(3f));
+        g2d.fillRect(5*32,2*32,32,32);
+        for (int i=0 ; i<=screenHeight ; i+=32){
+            g2d.drawLine(0,i,screenWidth,i);
+
+        }
+        for(int j=0 ; j<=screenWidth ; j+=32){
+            g2d.drawLine(j,0,j,screenHeight);
+        }
 
     }
 
